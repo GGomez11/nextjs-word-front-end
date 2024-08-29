2fbc74fe59491d33ff4eca6a1beb24198f4169a0
Gregory Gomez <gregorygomez@Gregorys-MBP.attlocal.net>
2024-08-29 15:29:10 -0500

    style: set navbar position to absolute

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,6 +1,6 @@
 export default function Navbar() {
     return (        
-        <div className="w-full h-10 bg-white border-black border-b-1">
+        <div className="w-full h-10 bg-white border-black border-b-1 absolute">
             <div className="h-full flex justify-start items-center mx-6 py-4">
                 <p className="text-black">Words</p>
             </div>
