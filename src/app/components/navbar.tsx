27e0f0213ad1233698e895b11d9fa09b56946af2
Gregory Gomez <gregorygomez@Gregorys-MBP.attlocal.net>
2024-08-29 10:50:29 -0500

    feat: create navbar component

---
+++ src/app/components/navbar.tsx
@@ -0,0 +1,9 @@
+export default function Navbar() {
+    return (        
+        <div className="w-full h-10 bg-white border-black border-b-1">
+            <div className="h-full flex justify-start items-center mx-6 py-4">
+                <p className="text-black">Words</p>
+            </div>
+        </div>
+    )
+}
