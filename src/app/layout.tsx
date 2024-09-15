e44b6c66c95d10030352a0737045e6477bd68fb7
Gregory Gomez <gregorygomez@Gregorys-MBP.attlocal.net>
2024-09-15 15:48:03 -0500

    style: set height to full for layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,9 @@ export default function RootLayout({
     <html lang="en" className={myFont.className + ' h-full'}>
       <body className="h-full bg-gradient-to-t from-lowerGradient to-upperGradient">
         <Navbar/>
-        {children}
+        <div className="h-full">
+          {children}
+        </div>
       </body>
     </html>
   );
