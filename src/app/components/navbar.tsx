788d51ba862e25c9e2a93ca2fcc90fa4b7c47a9f
Gregory Gomez <gregorygomez@Gregorys-MBP.attlocal.net>
2024-09-15 15:59:56 -0500

    feat: add sign out button

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,8 +1,27 @@
-export default function Navbar() {
+import Button from "@mui/material/Button";
+import { doLogout } from "../actions";
+import { auth } from "@/app/auth";
+import Image from 'next/image';
+
+export default async function Navbar() {
+    const session = await auth();
+    
     return (        
         <div className="w-full h-10 bg-white border-black border-b-1 absolute">
-            <div className="h-full flex justify-start items-center mx-6 py-4">
+            <div className="h-full flex justify-between items-center mx-6 py-4">
                 <p className="text-black">Words</p>
+                { (session?.user) &&
+                    <form action={doLogout} className="flex justify-end">
+                        <Button type="submit" className="p-0 flex justify-end"> 
+                            <Image
+                                src="logout.svg"
+                                width={30}
+                                height={30}
+                                alt="Log out icon"
+                            />
+                        </Button>
+                    </form>
+                }
             </div>
         </div>
     )
