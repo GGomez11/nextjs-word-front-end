737f32c814a7d0a6292a3a938d7c44138540ecdb
Gregory Gomez <gregorygomez@Gregorys-MBP.attlocal.net>
2024-10-01 19:22:12 -0500

    style: add styling to wordCard component

--- src/app/components/wordcard.tsx
+++ src/app/components/wordcard.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react"
 
-export default function WordCard() {
+export default function WordCard({className = ''}) {
     const [isFlipped, setIsFlipped] = useState(false);
     const [isFlipping, setIsFlipping] = useState(false);
     const [displayText, setDisplayText] = useState(false);
@@ -35,18 +35,18 @@ export default function WordCard() {
     };
 
     return (
-        <div onClick={handleFlip} className={`flex text-black flex-col bg-white rounded-lg zeroWidth:w-2/3 md:w-3/5 lg:w-4/5 h-44 md:h-52 p-5
+        <div onClick={handleFlip} className={`${className} flex text-black flex-col bg-white p-4 rounded-lg zeroWidth:w-4/5 md:w-3/5 lg:w-4/5 h-44 md:h-52 p-5
        shadow-sm cursor-pointer transition-transform duration-1000 delay-1500 ${isFlipped ? 'rotate-y-180' : ''}`}>
-            <div className={`flex flex-col justify-center w-full w-full h-full bg-white items-center rounded-lg  ${isFlipped ? 'hidden' : ''}`}>
+            <div className={`flex flex-col justify-evenly w-full w-full h-full bg-white items-center rounded-lg  ${isFlipped ? 'hidden' : ''}`}>
                 <div>
-                    {!displayText && <h1 className="text-xl md:text-2xl">{word.word}</h1>}
+                    {!displayText && <h1 className="text-xl zeroWidth:text-base md:text-2xl">{word.word}</h1>}
                 </div>
-                <div className="flex w-fit justify-center mt-4 px-2 overflow">
-                    {!displayText && <p>{word.defintion}</p>}
+                <div className="flex w-fit justify-center overflow">
+                    {!displayText && <p className="zeroWidth:text-base">{word.defintion}</p>}
                 </div>
             </div>
             <div className={`w-full h-full bg-white flex justify-center items-center rounded-lg rotate-y-180 ${isFlipped ? '' : 'hidden'}`}>
-                {displayText && <span className="text-black">Back Side</span>} 
+                {displayText && <p className="text-black zeroWidth:text-base">Back Side</p>} 
             </div> 
         </div>
     )
