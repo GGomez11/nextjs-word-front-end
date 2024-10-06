70efd5b6cdf134618491dcf5576681a97785bbac
Gregory Gomez <gregorygomez@Gregorys-MBP.attlocal.net>
2024-10-06 10:01:12 -0500

    feature: create home page

---
+++ src/app/home/page.tsx
@@ -0,0 +1,38 @@
+import { auth } from "@/app/auth";
+import { redirect } from "next/navigation";
+import WordCard from "../components/wordCard";
+import SearchBar from "../components/searchBar";
+
+export default async function Home() {
+    const session = await auth();
+    
+    if (!session?.user) redirect("/");
+
+    return (
+        <div className="h-full flex flex-col justify-evenly items-center">
+            <SearchBar/>
+            <div className="pb-4 sm:text-xl md:text-2xl lg:text-3xl px-10 zeroWidth:hidden xl:block">Welcome {session?.user.name}, here are your words.</div>
+            <div className="grid zeroWidth:grid-cols-1 md:grid-cols-2 xl:grid-cols-4">
+                <div className="mb-10 flex flex-row justify-center w-full">
+                    <WordCard/> 
+                </div>
+                <div className="mb-10 flex flex-row justify-center w-full">
+                    <WordCard/>
+                </div>
+                <div className="mb-10 flex flex-row justify-center w-full">
+                    <WordCard/> 
+                </div>
+                <div className="mb-10 flex flex-row justify-center w-full">
+                    <WordCard/>
+                </div>
+                <div className="mb-10 flex flex-row justify-center w-full">
+                    <WordCard/> 
+                </div>
+                <div className="mb-10 flex flex-row justify-center w-full">
+                    <WordCard/>
+                </div>
+            </div>
+            <div className="">Navigation Buttons</div>
+        </div>
+    )
+}
