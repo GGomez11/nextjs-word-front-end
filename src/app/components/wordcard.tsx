6af5e4bb4dfa8e2ac611b55d072d793d8e81fcbd
Gregory Gomez <gregorygomez@Gregorys-MBP.attlocal.net>
2024-10-20 13:43:32 -0500

    feature: make arrows display slowly on hover of card

--- src/app/components/wordcard.tsx
+++ src/app/components/wordcard.tsx
@@ -2,15 +2,19 @@
 
 import { useState } from "react";
 import IconButton from '@mui/material/IconButton';
+import ArrowBack from "@mui/icons-material/ArrowBack";
+import ArrowForward from "@mui/icons-material/ArrowForward";
 import DeleteIcon from '@mui/icons-material/Delete';
 import Tooltip from '@mui/material/Tooltip';
 import { WordCardProps } from "../types/wordCardProps";
+
 
 export default function WordCard({className = '', word}: WordCardProps) {
     const [isFlipped, setIsFlipped] = useState(false);
     const [isFlipping, setIsFlipping] = useState(false);
     const [displayText, setDisplayText] = useState(false);
     const [displayedDefinition, setDisplayedDefinition] = useState(word.results[0])
+    const [currentIndex, setCurrentIndex] = useState(0);
 
     let counter = 0
     setInterval(() => {
@@ -33,13 +37,28 @@ export default function WordCard({className = '', word}: WordCardProps) {
         console.log('handle delete')
     }
 
+    const handleNextDefinition = (event: Event) => {
+        event.stopPropagation();
+        if (currentIndex + 1 < word.results.length) {
+            setDisplayedDefinition(word.results[currentIndex+1])
+            setCurrentIndex(currentIndex+1)        
+        }
+    }
+    const handlePreviousDefinition = (event: Event) => {
+        event.stopPropagation();
+        if (currentIndex - 1 >= 0) {
+            setDisplayedDefinition(word.results[currentIndex-1])
+            setCurrentIndex(currentIndex-1)
+        }
+    }
+
 
     return (
-        <div onClick={handleFlip} className={`${className} flex zeroWidth:w-96 xl:w-[290px] text-black flex-col bg-white px-4 rounded-lg w-full zeroWidth:h-44 md:h-52 lg:h-44 xl:h-[350px] xl:p-3
+        <div onClick={handleFlip} className={`${className} word-card flex zeroWidth:w-96 xl:w-[290px] text-black flex-col bg-white px-4 rounded-lg w-full zeroWidth:h-44 md:h-52 lg:h-44 xl:h-[350px] xl:p-3
        shadow-sm cursor-pointer transition-transform duration-1000 delay-1500 ${isFlipped ? 'rotate-y-180' : ''}`}>
             <div className={`relative flex flex-col justify-evenly xl:justify-start w-full w-full h-full bg-white items-center rounded-lg  ${isFlipped ? 'hidden' : ''}`}>
                 {!displayText &&
-                    <span className="absolute zeroWidth:-left-2 xl:left-0 zeroWidth:top-2 xl:top-0">1/{word.results.length}</span>
+                    <span className="absolute zeroWidth:-left-2 xl:left-0 zeroWidth:top-2 xl:top-0">{currentIndex+1}/{word.results.length}</span>
                 }
                 {!displayText &&
                 <Tooltip title="Remove word">
@@ -55,6 +74,14 @@ export default function WordCard({className = '', word}: WordCardProps) {
                 <div className="flex w-fit justify-center overflow">
                     {!displayText && <p className="zeroWidth:text-base">{displayedDefinition.definition}</p>}
                 </div>
+                {!displayText && <div><IconButton onClick={handlePreviousDefinition} className="absolute zeroWidth:top-1/3 xl:top-2/4 -left-1 arrows"> 
+                    <ArrowBack fontSize="inherit"/>
+                </IconButton>
+                <IconButton onClick={handleNextDefinition} className="absolute zeroWidth:top-1/3 xl:top-2/4 -right-1 arrows"> 
+                    <ArrowForward fontSize="inherit"/>
+                </IconButton>
+                </div>
+                }
             </div>
             <div className={`w-full h-full bg-white flex justify-center items-center rounded-lg rotate-y-180 ${isFlipped ? '' : 'hidden'}`}>
                 {displayText && 
