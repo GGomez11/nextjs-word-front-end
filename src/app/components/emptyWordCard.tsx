725c9517dc19f19290d3b0ca189398c3f397d766
Gregory Gomez <gregorygomez@Gregorys-MBP.attlocal.net>
2024-10-21 09:09:45 -0500

    style: make width same between empty and regular cards

--- src/app/components/emptyWordCard.tsx
+++ src/app/components/emptyWordCard.tsx
@@ -4,7 +4,7 @@ export default function WordCard() {
 
     return (
         <Tooltip title="Add a word">
-            <div className="flex justify-center items-center text-black flex-col bg-white w-full px-4 rounded-lg zeroWidth:h-44 md:h-52 lg:h-44 xl:h-[350px] 
+            <div className="flex justify-center items-center text-black flex-col bg-white zeroWidth:w-96 xl:w-[290px] px-4 rounded-lg zeroWidth:h-44 md:h-52 lg:h-44 xl:h-[350px] 
                 shadow-sm cursor-pointer transition-transform duration-1000 delay-1500">
                 <p className="text-8xl relative -top-2">+</p>
             </div>
